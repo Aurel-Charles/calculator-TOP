aba2a1ee6f1eec32059c61a5c35177da079675dd
Aurel-Charles <charlesaurelienpro@gmail.com>
2025-09-27 13:17:15 +0200

    Add key board support
    create seperat function triggerd whenever we click or press a button

--- script.js
+++ script.js
@@ -47,6 +47,9 @@ function operate(firstNum, operator, secondNum) {
         case 'x':
             return roundResult((Number(firstNum) * Number(secondNum)))
             break;
+        case '*':
+            return roundResult((Number(firstNum) * Number(secondNum)))
+            break;
         case '/':
             if (secondNum == 0) {
                 return "ERROR"
@@ -62,69 +65,70 @@ function operate(firstNum, operator, secondNum) {
 
 
 
-
+//////////////////////////////////////////////////////////////////////////////////////////
 // functions that populate the display
-const digitNum = document.querySelectorAll('.digit-btn')
+const digitBtn = document.querySelectorAll('.digit-btn')
 const display = document.querySelector('#result')
 
 
 let displayNumber = 0
 let newArrNumber = [] // array to concatenate each number pushed in it
 
-function populateDisplay() {
-    digitNum.forEach(element => {
-        element.addEventListener('click', e => {
-            console.log(e.target.textContent);
-            console.log(pointAvailable);
-            
-            if (e.target.textContent == '.' && pointAvailable) {
-                newArrNumber.push(e.target.textContent)
-                let newNumber = newArrNumber.join('')
-                display.textContent = newNumber
-                displayNumber = Number(newNumber) // store the new number into the variable 'displayNumber'
-                console.log('Number on the display: ' + displayNumber);
-                operatorAvailable = true
-                pointAvailable = false 
-            }else if (e.target.textContent == '.' && !pointAvailable) {
-                return             
-            }else if (e.target.textContent != '.') {
-                newArrNumber.push(e.target.textContent)
-                let newNumber = newArrNumber.join('')
-                display.textContent = newNumber
-                displayNumber = Number(newNumber) // store the new number into the variable 'displayNumber'
-                console.log('Number on the display: ' + displayNumber);
-                operatorAvailable = true
-            }
-
-            
-        })
-    });
+// this function populate the display
+function populateDisplay(arrValue) {
+    if (newArrNumber.length == 10) {
+        return
+    }
+    if (arrValue == '.' && pointAvailable) {
+        newArrNumber.push(arrValue)
+        let newNumber = newArrNumber.join('')
+        display.textContent = newNumber
+        displayNumber = Number(newNumber) // store the new number into the variable 'displayNumber'
+        console.log('Number on the display: ' + displayNumber);
+        operatorAvailable = true
+        pointAvailable = false 
+    }else if (arrValue == '.' && !pointAvailable) {
+        return             
+    }else if (arrValue != '.') {
+        newArrNumber.push(arrValue)
+        let newNumber = newArrNumber.join('')
+        display.textContent = newNumber
+        displayNumber = Number(newNumber) // store the new number into the variable 'displayNumber'
+        console.log('Number on the display: ' + displayNumber);
+        operatorAvailable = true
+    }
     
 }
 
-populateDisplay()
+/// add key board supports (typing a number or '.') and then trigger the populate function to update the display
+document.addEventListener('keypress', function (event) {
+    let arrDigit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.']
+    for (let i = 0; i < arrDigit.length; i++) {
+        if (arrDigit[i] == event.key) {
+            console.log( 'Next array value is: ' + event.key);
+            populateDisplay(event.key)  
+        }   
+    }
+})
+
+
+// add event listener  for clicking on a digit ou '.' / ',' and then trigger the populate function to update the display
+digitBtn.forEach(element => {
+    element.addEventListener('click', e => {
+        console.log(e.target.textContent);
+        console.log(pointAvailable);
+        console.log( 'Next array value is: ' + e.target.textContent);
+        populateDisplay(e.target.textContent)
+    })
+});
 
 
 
-
-
-// ////// check the value of displayNumber each time a btn number is pressed ONLY FOR THE CONSOLE
-// digitNum.forEach(element => {
-//         element.addEventListener('click', e => {
-//             console.log( 'DisplayNumber to become the stored: ' + displayNumber);                         
-//         })
-//     })
-// /////
-
-
-
-
-// manage the event every time we click on a operator-btn
-const operatorBtn = document.querySelectorAll('.operator-btn')
-operatorBtn.forEach(element => {
-    element.addEventListener('click', e => {   
-        if (firstNum === undefined && operatorAvailable) {
-            operator = e.target.textContent
+//////////////////////////////////////////////////////////////////////////////////////////
+//function to manage operation when an operator is set
+function operatorTrigged(operatorTrigged) {
+    if (firstNum === undefined && operatorAvailable) {
+            operator = operatorTrigged
             firstNum = parseFloat(displayNumber)
             console.log('First num: ' + firstNum);
             console.log('Operator is: ' + operator);
@@ -134,7 +138,7 @@ operatorBtn.forEach(element => {
             operatorAvailable = false
             
         }else if (firstNum !== undefined && operatorAvailable){
-            nextOperator = e.target.textContent
+            nextOperator = operatorTrigged
             secondNum = parseFloat(displayNumber)
             result =  operate(firstNum, operator , secondNum)
             
@@ -149,15 +153,36 @@ operatorBtn.forEach(element => {
             operatorAvailable = false
             display.textContent = result
         }else if (firstNum !== undefined && !operatorAvailable) {
-            operator = e.target.textContent
+            operator = operatorTrigged
             console.log( 'New operator set: ' + operator); 
         }
         pointAvailable = true
-    })
+}
 
+
+// manage the event every time we CLICK on a operator-btn
+const operatorBtn = document.querySelectorAll('.operator-btn')
+operatorBtn.forEach(element => {
+    element.addEventListener('click', e => {   
+        operatorTrigged(e.target.textContent)
+    })
 });
 
+// manage the event every time we TYPE  on a operator-btn
+document.addEventListener('keypress', function (event) {
+    let arrDigit = ['+', '-', "*",'x', '/']
+    for (let i = 0; i < arrDigit.length; i++) {
+        if (arrDigit[i] == event.key) {
+            console.log( 'Next array value is: ' + event.key);
+            operatorTrigged(event.key)  
+        }   
+    }
+})
 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
 // manage the event every time we click on the clear-btn
 const clearBtn = document.querySelector('.clear-btn')
 clearBtn.addEventListener('click', e => {
@@ -179,10 +204,8 @@ clearBtn.addEventListener('click', e => {
 
 
 
-
-// manage the event every time we click on the equal-btn
-const equalBtn = document.querySelector('.equal-btn')
-equalBtn.addEventListener('click', e => { 
+//////////////////////////////////////////////////////////////////////////////////////////
+function equal() {
     console.log(operatorAvailable);
     
     if (firstNum == undefined) {
@@ -198,49 +221,50 @@ equalBtn.addEventListener('click', e => {
         pointAvailable = true
         return display.textContent = result
     }
+}
+
+// manage the event every time we click on the equal-btn
+const equalBtn = document.querySelector('.equal-btn')
+equalBtn.addEventListener('click', e => { 
+    equal()
+})
+
+// manage the event every time we PRESS 'ENTER'
+document.addEventListener('keypress', function (event) {
+    console.log(event.key);
+    if (event.key == 'Enter') {
+        equal()
+    } 
 })
 
 
 
-// manage the event every time we click on the backspace-btn
-const backspaceBtn = document.querySelector('.backspace-btn')
-backspaceBtn.addEventListener('click', e => {
+//////////////////////////////////////////////////////////////////////////////////////////
+// function the handle the backspace purpose
+function backspaceTrigged() {
     if (newArrNumber[newArrNumber.length] == '.' ) {
-        pointAvailable == true
+    pointAvailable == true
     }
     newArrNumber.pop()
     let newNumber = newArrNumber.join('')
     display.textContent = newNumber
     displayNumber = Number(newNumber) // store the new number into the variable 'displayNumber'
     console.log('Number on the display: ' + displayNumber);
+}
 
-    
+
+// manage the event every time we click on the backspace-btn
+const backspaceBtn = document.querySelector('.backspace-btn')
+backspaceBtn.addEventListener('click', e => {
+    backspaceTrigged()
+})
+
+// manage the event every time we PRESS 'Backspace'
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'Backspace') {
+        backspaceTrigged()
+    } 
 })
 
 
-
-
-
-
-
-
-
-
-
-
-// entre un numero 
-    //s'affiche sur le display
-    // devien second Number
-// click operator
-    // first number 0 , 
-    // store store seconde = display number
-    // operate avec zero 
-    // result => first number 
-// entre second numero
-// click operator  resultat du premier calcul ,
-// entre un nouveau numero
-
-
-
-
-
+   
